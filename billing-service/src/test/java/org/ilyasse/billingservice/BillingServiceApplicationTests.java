0f755b9b06249d2b84a8f5d7fe3dfe162b78ae65
ilyasseyounes <ilyasseyounes1@gmail.com>
2025-10-10 10:38:56 +0100

    Add billing service module with initial configuration and setup

---
+++ billing-service/src/test/java/org/ilyasse/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ilyasse.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests{
+
+    @Test
+    void contextLoads () {
+    }
+
+}
